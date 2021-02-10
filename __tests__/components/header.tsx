7c4ba4919785070c03046841a4ec167904dc9c57
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2021-02-10 17:42:37 +0100

    Added SEO testing

--- __tests__/components/header.tsx
+++ __tests__/components/header.tsx
@@ -1,11 +1,14 @@
-import '@testing-library/jest-dom'
-
-import React from "react"
-import Header from "../../src/components/layout/header"
-import {render, fireEvent, screen} from '@testing-library/react'
+import React from "react";
+import Header from "../../src/components/layout/header";
+import {render, fireEvent, screen} from '@testing-library/react';
 
 
 test('check the logo', () => {
   render(<Header />)
   expect(screen.queryByAltText("Logo Icon")).toBeInTheDocument();
 })
+
+// test('check exact three links', () => {
+//   render(<Header />)
+//   expect(screen.queryByAltText("Logo Icon")).toBeInTheDocument();
+// })
